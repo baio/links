39663e5ed083421d8486d51b081b4afd93056dd8
baio <max.putilov@gmail.com>
2013-04-19 11:57:08 +0400

    gephi converter

--- contrib2gexf.py
+++ contrib2gexf.py
@@ -16,7 +16,7 @@ def get_edges(in_txt):
                     link_types = attrs_spt[0].split('.')
                     url = attrs_spt[1] if len(attrs_spt) == 2 else perv_url
                     perv_url = url
-                    yield (name_1, name_2, link_types, url)
+                    yield (name_1.strip('\r\n'), name_2.strip('\r\n'), [x.strip('\r\n') for x in link_types], url.strip('\r\n'))
 
 def get_elements(in_txt):
     edges = list(get_edges(in_txt))
@@ -26,40 +26,65 @@ def get_elements(in_txt):
         nodes.append(name)
     return nodes, edges
 
+def get_attr_rel_for(rel):
+    return {
+        u"брат" : "family_rel",
+        u"муж" : "family_rel",
+        u"двоюрод" : "family_rel",
+        u"замуж" : "family_rel",
+        u"сын" : "family_rel",
+        u"друг" : "private_rel",
+        u"кореш" : "private_rel",
+        u"служба" : "prof_rel",
+        u"лобби" : "prof_rel",
+        u"партнер" : "prof_rel"
+
+    }[rel];
+
+
 def get_xml_elements(in_txt):
     nodes, edges = get_elements(in_txt)
     node_els = []
     edge_els = []
     for node in nodes:
         node_el = ET.Element("node")
-        node_el.attrib["id"] = node.replace(" ", "_")
-        node_el.attrib["label"] = node
+        node_el.attrib["id"] = node.replace(" ", "_").strip()
+        node_el.attrib["label"] = node.strip()
         node_els.append(node_el)
     for edge in edges:
         edge_el = ET.Element("edge")
         edge_el.attrib["source"] = edge[0].replace(" ", "_")
         edge_el.attrib["target"] = edge[1].replace(" ", "_")
-        edge_el.attrib["label"] = u"{};{}".format(",".join(edge[2]), edge[3])
+        attrs_el = ET.Element("attvalues")
+        edge_el.append(attrs_el)
+        for rel in edge[2]:
+            attr_val_el = ET.Element("attvalue")
+            attrs_el.append(attr_val_el)
+            attr_val_el.attrib["for"] = get_attr_rel_for(rel)
+            attr_val_el.attrib["value"] = rel
+        attr_val_el = ET.Element("attvalue")
+        attrs_el.append(attr_val_el)
+        attr_val_el.attrib["for"] = "url"
+        attr_val_el.attrib["value"] = edge[3]
         edge_els.append(edge_el)
     return node_els, edge_els
 
 def merge_xml_elements(in_txt, merge_xml):
+    def ns_tag(tag):
+        return str(ET.QName('http://www.gexf.net/1.2draft', tag) )
+
     nodes, edges = get_xml_elements(in_txt)
     ET.register_namespace("", "http://www.gexf.net/1.2draft")
     parser = ET.XMLParser(encoding="utf-8")
     xml = ET.parse(merge_xml, parser)
     xml_root = xml.getroot()
-    xml_nodes = xml_root[1][0]
-    xml_edges = xml_root[1][1]
+    xml_nodes = xml_root.find(ns_tag("graph")).find(ns_tag("nodes"))
+    xml_edges = xml_root.find(ns_tag("graph")).find(ns_tag("edges"))
     for node in nodes:
         xml_nodes.append(node)
     for edge in edges:
         xml_edges.append(edge)
     xml.write("_" + merge_xml, "utf-8", xml_declaration=True)
-
-
-
-
 
 merge_xml_elements("contribs/baio_130418.txt", "template.gexf")
 

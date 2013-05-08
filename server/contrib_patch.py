a5629d0f19a890459c770298097293dd294af36e
baio <max.putilov@gmail.com>
2013-05-08 19:14:40 +0400

    fix

--- server/contrib_patch.py
+++ server/contrib_patch.py
@@ -3,22 +3,25 @@ from dom.contrib.patch import patch
 from es.elastic_search import append_names_tags
 
 def contrib_patch(user_name, contrib_id, items):
-    _res = patch(user_name, contrib_id, items)
-    res = zip(_res, items)
-    res = filter(lambda x: len(x[0]["err"]) == 0, res)
+    res = patch(user_name, contrib_id, items)
+    _items = []
+    for i, item in enumerate(items):
+        if len(res[i]["err"]) == 0 and item["_isRemoved"] == False:
+            _items.append(item)
     names = sum(map(lambda x: [
-       {"fname": x[1]["name_1"].split(' ')[0], "lname": x[1]["name_1"].split(' ')[1]},
-       {"fname": x[1]["name_2"].split(' ')[0], "lname": x[1]["name_2"].split(' ')[1]}
+       {"fname": x["name_1"].split(' ')[0], "lname": x["name_1"].split(' ')[1]},
+       {"fname": x["name_2"].split(' ')[0], "lname": x["name_2"].split(' ')[1]}
         ]
-        , res), [])
+        , items), [])
     tags = sum(map(lambda x:[
-       {"name": x[1]["private_rel"], "type": "private"},
-       {"name": x[1]["prof_rel"], "type": "prof"},
-       {"name": x[1]["family_rel"], "type": "family"}
-       ], res), [])
-    tags = filter(lambda x: x["name"] is not None, tags)
+       {"name": x["private_rel"], "type": "private"},
+       {"name": x["prof_rel"], "type": "prof"},
+       {"name": x["family_rel"], "type": "family"}
+       ], items), [])
+    tags = filter(lambda x: x["name"], tags)
     #TODO append only unique items
-    append_names_tags(names, tags)
-    return _res
+    if len(names) > 0 and len(tags) > 0:
+        append_names_tags(names, tags)
+    return res
 
 

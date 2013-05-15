f7169315bd6cb9d5c6b53494e3a6d9ba0e980502
baio <max.putilov@gmail.com>
2013-05-15 14:57:42 +0400

    pub not yours

--- dom/contrib/get_all.py
+++ dom/contrib/get_all.py
@@ -3,10 +3,10 @@ import pymongo as mongo
 from config.config import config
 from  bson.objectid import ObjectId
 
-def get(user_name):
+def get(user_id):
     client = mongo.MongoClient(config["MONGO_URI"])
     db = client[config["MONGO_DB"]]
-    user = db.users.find({}, {"contribs" : 1}).limit(50)
+    user = db.users.find({"_id" : {"$ne" : user_id}}, {"contribs" : 1}).limit(50)
     return sum(map(lambda x: x["contribs"], user), [])
 
 

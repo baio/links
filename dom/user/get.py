2e154b52a0db548a7c13c9c9abab00357f37886b
baio <max.putilov@gmail.com>
2013-05-25 14:42:33 +0400

    KT-45

--- dom/user/get.py
+++ dom/user/get.py
@@ -6,4 +6,8 @@ def get(user_id):
     client = mongo.MongoClient(config["MONGO_URI"])
     db = client[config["MONGO_DB"]]
     user = db.users.find_one({"_id": user_id})
+    for graph in user["graphs"]:
+        contribs = list(graph["contribs"])
+        graph["contribs"] = [filter(lambda x: x["ref"] == contrib, user["contribs"])[0] for contrib in contribs]
+        
     return user

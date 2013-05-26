f799f59b3760581db7948481c7f262b7e04daa81
baio <max.putilov@gmail.com>
2013-05-26 13:25:05 +0400

    fixes

--- dom/user/get.py
+++ dom/user/get.py
@@ -6,8 +6,9 @@ def get(user_id):
     client = mongo.MongoClient(config["MONGO_URI"])
     db = client[config["MONGO_DB"]]
     user = db.users.find_one({"_id": user_id})
+    user["contribs"] = user["contribs"][::-1]
     for graph in user["graphs"]:
-        contribs = list(graph["contribs"])
+        contribs = graph["contribs"][::-1]#list(graph["contribs"])[::-1]
         graph["contribs"] = [filter(lambda x: x["ref"] == contrib, user["contribs"])[0] for contrib in contribs]
 
     return user

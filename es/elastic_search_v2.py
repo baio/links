723daa387842fd8633807bcb42baf37c10b5ad2a
baio <max.putilov@gmail.com>
2013-06-02 12:07:30 +0400

    KT-76

--- es/elastic_search_v2.py
+++ es/elastic_search_v2.py
@@ -11,7 +11,8 @@ _elastic_host_url = os.getenv("ES_URI", None)
 """
 index - set type
 type - field type
-list with buckets: (key, val)
+l - list with buckets: (key, val)
+val_field_name = "val", name of the value field in es
 """
 def mset(index, type, l, val_field_name = "val"):
     if not _elastic_host_url or len(l) == 0: return []
@@ -24,3 +25,16 @@ def mset(index, type, l, val_field_name = "val"):
     res = req.post(_elastic_host_url + "/_bulk", data=d)
     content = yaml.load(res.content)
     return map(lambda x: x["index"]["ok"], content["items"])
+
+def get(index, type, val, val_field_name = "val", search_val_field_name = "val.autocomplete"):
+    if not _elastic_host_url: return []
+    d = { "query" : {
+            "fuzzy" : {
+                search_val_field_name : val
+            }
+        }
+    }
+    res = req.post(_elastic_host_url + "/" + index + "/" + type + "/_search", data=json.dumps(d))
+    hits = yaml.load(res.content)["hits"]
+    hits = hits["hits"] if len(hits) > 0 else []
+    return map(lambda x: (x["_id"], x["_source"][val_field_name]), hits)
--- tests/es_test_v2.py
+++ tests/es_test_v2.py
@@ -7,8 +7,13 @@ from es import elastic_search_v2 as es
 class TestContribElastic_v2(unittest.TestCase):
 
     def test_mset(self):
-        names = [(u"max", "max"), ("mikkey", "mikkey"), ("hommer", "hommer")]
-        es.mset("name", "gov-ru", names)
+        return
+        names = [(u"max pain", "max pain"), ("mikkey mauase", "mikkey mause"), ("hommer simpson", "hommer simpson")]
+        es.mset("gov-ru[name]", "name", names)
+
+    def test_get(self):
+        res = es.get("gov-ru[name]", "name", "mix")
+        print res
 
 if __name__ == '__main__':
     unittest.main()
